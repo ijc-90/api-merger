d2d8e5b1a8d89dc2fc4ab8a3ed415ce45edd168d
Joaquin <joaquin.consoni@agileengine.com>
2021-08-17 00:51:06 -0300

    documentation and changes on mockup values

--- src/external_integration.py
+++ src/external_integration.py
@@ -5,23 +5,23 @@ def call_external_api(api, member_id):
     #mockup calls
     if api == 'https://api1.com':
         if member_id == 1:
-            return {'deductible': 1100, 'stop_loss': 11000, 'oop_max': 5100}
+            return {'deductible': 1190, 'stop_loss': 11020, 'oop_max': 5103}
         if member_id == 2:
-            return {'deductible': 1120, 'stop_loss': 12000, 'oop_max': 5220}
+            return {'deductible': 1120, 'stop_loss': 12300, 'oop_max': 5250}
         if member_id == 3:
-            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 30}
+            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 310}
     if api == 'https://api2.com':
         if member_id == 1:
-            return {'deductible': 1100, 'stop_loss': 11000, 'oop_max': 5100}
+            return {'deductible': 1101, 'stop_loss': 11010, 'oop_max': 5102}
         if member_id == 2:
-            return {'deductible': 1120, 'stop_loss': 12000, 'oop_max': 5220}
+            return {'deductible': 1121, 'stop_loss': 12300, 'oop_max': 5220}
         if member_id == 3:
-            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 30}
+            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 302}
     if api == 'https://api3.com':
         if member_id == 1:
-            return {'deductible': 1100, 'stop_loss': 11000, 'oop_max': 5100}
+            return {'deductible': 1110, 'stop_loss': 11090, 'oop_max': 5101}
         if member_id == 2:
-            return {'deductible': 1120, 'stop_loss': 12000, 'oop_max': 5220}
+            return {'deductible': 1126, 'stop_loss': 12300, 'oop_max': 5210}
         if member_id == 3:
-            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 30}
+            return {'deductible': 3100, 'stop_loss': 100, 'oop_max': 304}
     return default
